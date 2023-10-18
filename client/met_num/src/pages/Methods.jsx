df33ba35274856b0bcb223199a09d673ad22feda
gabcardona0782 <gabcardona0782@gmail.com>
2023-10-18 17:49:42 -0500

    Configuraciones básicas de rutas

--- client/met_num/src/pages/Methods.jsx
+++ client/met_num/src/pages/Methods.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import {useState} from 'react'
 
 function Methods() {
+    const [state, setState] = useState(0);
     return (
         <h1 className='text-3xl font-bold'>
             Methods Page
